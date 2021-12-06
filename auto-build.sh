d05e04e4e8fe177473c508b9b92354279c16c570
TaraKit <darakit069@gmail.com>
2021-12-06 11:04:09 +0700

    update script

--- auto-build.sh
+++ auto-build.sh
@@ -3,16 +3,16 @@
 
 mvn clean install
 
-docker build -t ${USER} .
+sudo docker build -t ${USER} .
 
 #remove old container
-docker container rm -f ${USER}-api
+sudo docker container rm -f ${USER}-api
 sleep 2s
 #remove hanging image last built <none>
-docker rmi $(sudo docker images -f "dangling=true" -q) --force
+sudo docker rmi $(sudo docker images -f "dangling=true" -q) --force
 sleep 2s
 
-docker run -d --name ${USER}-api -p 8080:8080 ${USER}
+sudo docker run -d --name ${USER}-api -p 8080:8080 ${USER}
 #sudo docker run -d --name ${USER}-api -p 8080:8080 -v /home/homestore/images:/images ${USER}
 
 echo "Success..."
